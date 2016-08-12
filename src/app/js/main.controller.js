928b82e6884191dce1c8d1f60bed430e849b15da
Scott O'Brien <sobrien@seamgen.com>
2016-08-12 16:53:06 -0700

    animate fix

--- src/app/js/main.controller.js
+++ src/app/js/main.controller.js
@@ -1,17 +1,17 @@
 angular.module('app')
     .controller('MainCtrl', function($log, GoogleAuthService, UserService, $rootScope, $state, $timeout, $scope, SpinnerService) {
         var vm = this;
-        $scope.albumList = [];
+        //$scope.albumList = [];
 
         GoogleAuthService.loginCheck();
 
-        $log.debug();
+        //$log.debug('testing', UserService.albumArr.$loaded());
 
         //Check user and everything..
         UserService.userCheck().then(function(result){
           if (result.$value === null) {
             UserService.albumListInit().then(function(result) {
-              //$log.debug('albumIniting', result);
+              $log.debug('albumIniting', result);
               $scope.albumList = result;
               return;
             }).catch(function(error) {
@@ -19,10 +19,10 @@ angular.module('app')
             });
             return;
           }
-          UserService.albumArr.$loaded(function(result) {
-            //$log.debug('Album Arr', result, UserService.albumArr.$value);
+          UserService.albumArr.$loaded().then(function(result) {
             $scope.albumList = result;
             SpinnerService.spinnerOff();
+            $log.debug('Album Arr TODO', result);
           }).catch(function(error) {
             $log.debug('albumArr Error', error);
           });
