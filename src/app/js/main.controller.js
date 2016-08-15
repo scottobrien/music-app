5d0de57f8728f8751a1b3c2323d7939f1e14ac3d
Scott O'Brien <sobrien@seamgen.com>
2016-08-15 14:24:32 -0700

    added a temporary fix for loading array at login

--- src/app/js/main.controller.js
+++ src/app/js/main.controller.js
@@ -1,33 +1,32 @@
 angular.module('app')
-    .controller('MainCtrl', function($log, GoogleAuthService, UserService, $rootScope, $state, $timeout, $scope, SpinnerService) {
-        var vm = this;
-        //$scope.albumList = [];
+    .controller('MainCtrl', function($log, GoogleAuthService, UserService, $rootScope, $state, $timeout, $scope, SpinnerService, $window) {
+      var vm = this;
 
-        GoogleAuthService.loginCheck();
+      GoogleAuthService.loginCheck();
 
-        //$log.debug('testing', UserService.albumArr.$loaded());
-
-        //Check user and everything..
-        UserService.userCheck().then(function(result){
-          if (result.$value === null) {
-            UserService.albumListInit().then(function(result) {
-              $log.debug('albumIniting', result);
-              $scope.albumList = result;
-              return;
-            }).catch(function(error) {
-              $log.debug('albumInit Error', error);
-            });
-            return;
-          }
-          UserService.albumArr.$loaded().then(function(result) {
+      //Check user and everything..
+      UserService.userCheck().then(function(result) {
+        if (result.$value === null) {
+          UserService.albumListInit().then(function(result) {
+            $log.debug('albumIniting', result);
             $scope.albumList = result;
-            SpinnerService.spinnerOff();
-            $log.debug('Album Arr TODO', result);
+            return;
           }).catch(function(error) {
-            $log.debug('albumArr Error', error);
+            $log.debug('albumInit Error', error);
           });
+          return;
+        }
+
+        UserService.getAlubumArr().then(function(result) {
+          if (result.length === 0) { //Work around for now, fix in future..
+            $window.location.reload();
+          }
+          $scope.albumList = result;
+          SpinnerService.spinnerOff();
+        }).catch(function(error) {
+          $log.debug('albumArr Error', error);
         });
+      });
 
-        $log.debug('MainCtrl');
-
+      $log.debug('MainCtrl');
     });
